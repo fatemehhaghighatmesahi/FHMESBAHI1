7ee143f9dba76100913df8226a8ed65360bd88d6
fatemehhaghighatmesahi <109865910+fatemehhaghighatmesahi@users.noreply.github.com>
2022-08-23 00:18:04 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fatemeh HM
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/FatemehHM/pen/dymgyNz](https://codepen.io/FatemehHM/pen/dymgyNz).
+
---
+++ src/script.js
@@ -0,0 +1,12 @@
+
+var click__count;
+
+
+click__count = 0;
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  click__count = (typeof click__count === 'number' ? click__count : 0) + 1;
+  event.target.innerText = click__count;
+
+});
